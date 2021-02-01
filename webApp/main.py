42f1cebe0413bf062b9ba0a0b93e2fd1209ab9ee
optimho <michael@duplessis.nz>
2021-02-01 21:47:05 +1300

    initial

--- webApp/main.py
+++ webApp/main.py
@@ -4,8 +4,24 @@ from webApp.forms import LoginForm
 import webbrowser
 import webview
 main = Blueprint('main', __name__)
-
 webbrowser.open("127.0.0.1:5000", new=0, autoraise=True)
+
+labels = [
+    'JAN', 'FEB', 'MAR', 'APR',
+    'MAY', 'JUN', 'JUL', 'AUG',
+    'SEP', 'OCT', 'NOV', 'DEC'
+]
+
+values = [
+    967.67, 1190.89, 1079.75, 1349.19,
+    2328.91, 2504.28, 2873.83, 4764.87,
+    4349.29, 6458.30, 9907, 16297
+]
+
+colors = [
+    "#F7464A", "#46BFBD", "#FDB45C", "#FEDCBA",
+    "#ABCDEF", "#DDDDDD", "#ABCABC", "#4169E1",
+    "#C71585", "#FF4500", "#FEDCBA", "#46BFBD"]
 
 @main.route('/')
 @main.route('/index')
@@ -61,6 +77,9 @@ def login():
     return render_template('login.html', title='Sign In', form=form)
 
 
-@main.route('/plot', methods=['GET', 'POST'])
-def plot():
-    return render_template('plot.html', title='Plot')
+@main.route('/plot')
+def bar():
+    bar_labels=labels
+    bar_values=values
+    return render_template('bar_chart.html', title='Bitcoin Monthly Price in USD', max=17000, labels=bar_labels, values=bar_values)
+
